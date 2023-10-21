b0f521e0fc882b0ad140e76987c8f62a96dd3bcb
Irtiza Ashraf <irtizaashraf802@gmail.com>
2023-10-21 20:06:37 +0500

    process

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-// function getWeather() {
-//       const apiKey = 'a14224e727cd4126862171510231610'; // Replace with your API key
-//       const city = document.getElementById('city-input').value;
-//       const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
-//       fetch(apiUrl)
-//             .then(response => response.json())
-//             .then(data => {
-//                   const weatherInfo = `Weather in ${city}: ${data.weather}<br>
-//                                   Temperature: ${data.main.temp}°C<br>
-//                                   Humidity: ${data.main.humidity}%`;
-//                   document.getElementById('weather-info').innerHTML = weatherInfo;
-//             })
-//             .catch(error => {
-//                   console.error('Error fetching weather data:', error);
-//                   document.getElementById('weather-info').innerHTML = 'Error fetching weather data. Please try again.';
-//             });
-// }
-
 
 const rightnow = new Date;
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
@@ -139,7 +120,7 @@ form.addEventListener('submit', (e) => {
             .catch((error) => {
                   // Handle errors, for example, display an error message
                   console.error(error);
-                  weatherDetail.style.color = "red";
-                  weatherDetail.innerHTML = `<p class"text-2xl">Data Not Found.</p>`;
+                //   weatherDetail.style.color = "red";
+                  weatherDetail.innerHTML = `<p class="data">Data Not Found.</p>`;
             });
 });
